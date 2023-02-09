d6f2cf8f6aba31635798732f084bfe8f4fcda600
David Asensi <b4nedhl@gmail.com>
2023-02-09 14:05:54 +0100

    commit

--- api/index.js
+++ api/index.js
@@ -1,19 +1,22 @@
-import { Hono } from 'hono'
-import leaderboard from '../db/leaderboard.json'
+import { Hono } from 'hono';
+import leaderboard from '../db/leaderboard.json';
+import { serveStatic } from 'hono/serve-static.module';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (ctx) => {
-	return ctx.json([
-		{
-			endpoint: '/leaderboard',
-			description: 'Returns the leaderboard'
-		}
-	])
-})
+app.get('/', (ctx) =>
+  ctx.json([
+    {
+      endpoint: '/leaderboard',
+      description: 'Returns the leaderboard',
+    },
+  ])
+);
+
 app.get('/leaderboard', (ctx) => {
-	return ctx.json(leaderboard)
-})
+  return ctx.json(leaderboard);
+});
 
+app.get('/static/*', serveStatic({ root: './'}))
 
-export default app
+export default app;
