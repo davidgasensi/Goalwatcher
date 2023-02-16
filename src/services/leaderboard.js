ffd1642fb1fef3348e1e9065372974e7930bfdc2
David Asensi <b4nedhl@gmail.com>
2023-02-16 02:42:14 +0100

    Leaderboard and team service

---
+++ src/services/leaderboard.js
@@ -0,0 +1,10 @@
+export const getLeaderboard = async () => {
+   try {
+      const response = await fetch('https://goalwatcher.davidasensi.workers.dev/leaderboard')
+      const leaderboard = await response.json();
+      return leaderboard
+   } catch (e) {
+      // enviar el error en una notificacion
+      return []
+   }
+}
---
+++ src/services/teams.js
@@ -0,0 +1,10 @@
+export const getAllTeams = async () => {
+   try {
+      const response = await fetch('https://goalwatcher.davidasensi.workers.dev/teams')
+      const teams = await response.json();
+      return teams
+   } catch (e) {
+      // enviar el error en una notificacion
+      return []
+   }
+}
