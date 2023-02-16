84d0bb7fe0f3a34b892ed975ffcabf05ef439fa5
David Asensi <b4nedhl@gmail.com>
2023-02-16 23:50:43 +0100

    Components astro

---
+++ src/services/players.js
@@ -0,0 +1,10 @@
+export const getPlayers = async () => {
+   try {
+      const response = await fetch('https://goalwatcher.davidasensi.workers.dev/players')
+      const players = await response.json();
+      return players
+   } catch (e) {
+      // enviar el error en una notificacion
+      return []
+   }
+}
---
+++ src/services/statsAll.js
@@ -0,0 +1,12 @@
+export const getStatsPlayers = async (stat) => {
+  try {
+    const response = await fetch(
+      'https://goalwatcher.davidasensi.workers.dev/stats/players/' + stat
+    );
+    const stats = await response.json();
+    return stats;
+  } catch (e) {
+    // enviar el error en una notificacion
+    return [];
+  }
+};
