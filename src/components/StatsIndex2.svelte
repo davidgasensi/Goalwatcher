201f8d5cebd2a4bab7ac07d21f3cba24827b0f4f
David Asensi <b4nedhl@gmail.com>
2023-02-16 23:58:47 +0100

    svelte comps

---
+++ src/components/StatsIndex2.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	let characterName;
+
+    onMount(async () => {
+        const response = await fetch('https://goalwatcher.davidasensi.workers.dev/leaderboard');
+        const character = await response.json();
+        console.log(character);
+        characterName = character.points;
+    });
+</script>
+
+<main>
+    {#if characterName === undefined}
+        Loading Character Name...
+    {:else}
+        {characterName}
+    {/if}
+</main>
