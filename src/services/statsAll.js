25b3d0a50c95cac2ea4c493c6b640151c12a5cb9
David Asensi <b4nedhl@gmail.com>
2023-02-22 22:17:55 +0100

    Stats page

--- src/services/statsAll.js
+++ src/services/statsAll.js
@@ -10,3 +10,16 @@ export const getStatsPlayers = async (stat) => {
     return [];
   }
 };
+
+export const getStatsTeams = async (stat) => {
+  try {
+    const response = await fetch(
+      'https://goalwatcher.davidasensi.workers.dev/stats/teams/' + stat
+    );
+    const stats = await response.json();
+    return stats;
+  } catch (e) {
+    // enviar el error en una notificacion
+    return [];
+  }
+};
